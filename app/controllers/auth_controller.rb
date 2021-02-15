f6a0c72dbe68b5c803914350eb6f82d2b0435b6a
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-15 09:29:08 -0300

    Add route for logout action

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,16 +1,16 @@
 class AuthController < ApplicationController
   def login
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(email: params[:email])
 
     if @user&.authenticate(params[:password])
       token_and_cookie(@user.id)
       render json: { user: @user }
     else
-      render json: { error: 'Invalid username or password' }
+      render json: { error: 'Invalid username or password' }, status: 404
     end
   end
 
-  def destroy
+  def logout
     cookies.delete(:jwt)
   end
 end
