7409f3101ca6ae9f68f0114d87b5b86ca5776f73
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-15 10:40:28 -0300

    Create car model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_104207) do
+ActiveRecord::Schema.define(version: 2021_02_15_133910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.integer "horsepower"
+    t.integer "range"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.bigint "car_id", null: false
+    t.text "image_data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["car_id"], name: "index_pictures_on_car_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -23,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_02_15_104207) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "pictures", "cars"
 end
