af0c0a80509af6a47f5ab96cef9b3531e82e40e4
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-15 07:54:54 -0300

    Setup application controller for managing authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::API
-    include ::ActionController::Cookies
+  include ::ActionController::Cookies
+  SECRET_KEY = Rails.application.secrets.secret_base_key.to_s
+
+  def encode_token(payload)
+    JWT.encode(payload, SECRET_KEY, 'HS256')
+  end
+
+  def authorized?
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
+  private
+
+  def decoded_token
+    jwt = cookies.signed[:jwt]
+    JWT.decode(jwt, SECRET_KEY, true, algorithm: 'HS256')
+  end
+
+  def logged_in_user
+    return unless decoded_token
+
+    user_id = decoded_token[0]['user_id']
+    @current_user = User.find_by(id: user_id)
+  end
+
+  def logged_in?
+    !!logged_in_user
+  end
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,17 +1,17 @@
 class AuthController < ApplicationController
-    def login
-        @user = User.find_by(username: params[:username])
-    
-        if @user && @user.authenticate(params[:password])
-          token = encode_token({user_id: @user.id})
-          cookies.signed[:jwt] = {value: token, httponly: true, expires: 1.week.from_now}
-          render json: {user: @user, token: token}
-        else
-          render json: {error: "Invalid username or password"}
-        end
-    end
+  def login
+    @user = User.find_by(username: params[:username])
 
-    def destroy
-        cookies.delete(:jwt)
+    if @user&.authenticate(params[:password])
+      token = encode_token({ user_id: @user.id })
+      cookies.signed[:jwt] = { value: token, httponly: true, expires: 1.week.from_now }
+      render json: { user: @user, token: token }
+    else
+      render json: { error: 'Invalid username or password' }
     end
+  end
+
+  def destroy
+    cookies.delete(:jwt)
+  end
 end
