77452a184312133edc2f9ee8ada46d110f33b3a8
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-25 12:48:35 -0300

    Add tests for picture model

---
+++ spec/models/picture_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require 'database_cleaner'
+
+RSpec.describe Picture, type: :model do
+    before :each do
+      @car = Car.create(model: 'Porsche', horsepower: '456', range:'567', price: '34000', category: 'electric', description: 'This is an electric car')
+      @picture = Picture.create(image: '', car_id: @car.id, category: 'profile')
+    end
+
+    it { should belong_to(:car) }
+  
+    it 'returns picture created' do
+      expect(@picture.id).to eql(@picture.id)
+    end
+end
