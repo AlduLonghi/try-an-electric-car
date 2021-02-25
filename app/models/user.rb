654b816b4391666f3e4f0b7d733910a60b64cd6c
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-25 11:15:22 -0300

    Add validation for email with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :appointments, dependent: :destroy
   validates :name, length: { minimum: 6 }
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :password, length: { minimum: 5 }
 end
