be48e82fb95b8554dde7c8d385f4b884a325fbbe
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-02-26 12:23:17 -0300

    Change cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://localhost:3000', 'https://electricar.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
